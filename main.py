5a6bbd6d91846b3dc53157c5f58f4d27ea0961ad
bogdanduminyuk <77934329bogdan@gmail.com>
2016-10-28 14:38:14 +0300

    modifying main.py as cool style

--- main.py
+++ main.py
@@ -1,56 +1,65 @@
 # coding: utf-8
 from argparse import ArgumentParser
-from actionRepeater import ActionRepeater
-from csvdict import CSVdict
+from csv_dict import CsvDict
+
 
 def writer(args):
+    pass
+
+
+"""
     AR = ActionRepeater(args.filename)
     AR.write(args.keyboard)
+"""
+
 
 def clicker(args):
+    pass
+
+
+"""
     AR = ActionRepeater(args.filename)
     AR.repeat(args.count)
+"""
 
-def argsInit():
+
+def args_init():
     # top-level parser
-    argparser = ArgumentParser(
+    arg_parser = ArgumentParser(
         description="A simple write/execute user actions repeater.")
-    subparsers = argparser.add_subparsers(title='Subcommands',
-                                          description='Valid subcommands',
-                                          help='Additional help')
-    argparser.add_argument('-f', '--filename',
-                              type=str,
-                              default='temp.csv',
-                              help='Name of file to write')
+    subparsers = arg_parser.add_subparsers(title='Sub_commands',
+                                           description='Valid sub_commands',
+                                           help='Additional help')
+    arg_parser.add_argument('-f', '--filename',
+                            type=str,
+                            default='temp.csv',
+                            help='Name of file to write')
 
     # parser for 'write' command
     parser_write = subparsers.add_parser('write',
-                            description='Command is used for writting user actions')
-    
-    
-    parser_write.add_argument('-k', '--keyboard',
-                              action='store_true',
-                              help='Use it if you want to catch the keyboard events')
+                                         description='Command is used for writing user actions')
+
     parser_write.set_defaults(func=writer)
 
     # parser for 'exec' command
     parser_exec = subparsers.add_parser('exec',
-                            help='Command for *.csv file executing')
+                                        help='Command for *.csv file executing')
     parser_exec.add_argument('-c', '--count',
                              type=int,
                              default=1,
                              help='Means how many times to repeat. Min value = 1 and Max value = Inf')
     parser_exec.set_defaults(func=clicker)
-    
-    return argparser.parse_args()
+
+    return arg_parser.parse_args()
+
 
 if __name__ == "__main__":
-    args = argsInit()
-    
+    args = args_init()
+
     try:
         args.func(args)
 
     except AttributeError:
-        actions = CSVdict(args.filename).get()
+        actions = CsvDict(args.filename).get()
         for action in actions:
             print(action)
